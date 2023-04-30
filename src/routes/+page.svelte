aae487a17d5b221bd381c0adeb35ef29ce9bde9e
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-04-30 23:44:46 +0200

    úush

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 
 	import Aktuality from "../komponenty/web/Aktuality.svelte";
+	import Carousel from "../komponenty/web/Carousel.svelte";
 	import Karty from "../komponenty/web/Karty.svelte";
 import ONas from "../komponenty/web/O-nas.svelte";
         
 
 </script>
 
-<h1 class="text-3xl font-bold text-center text-black">Váš praktický lékař pro děti a dorost.</h1>
+<h1 class="text-6xl font-bold text-center text-black">Váš praktický lékař pro děti a dorost.</h1>
 
 <ONas/>
 <Aktuality/>
 <Karty/>
+<h1 class="text-3xl mt-16 font-bold text-center text-black">Seznam smluvních pojišťoven</h1>
+<div class="w-full">
+    <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/pji.png?alt=media&token=8d3e2883-df3a-4c14-8ae7-ab1000eed008" alt="Děti" class="w-1591.2 h-451 object-contain">
+</div>
+  
 <footer>
 
 </footer>
