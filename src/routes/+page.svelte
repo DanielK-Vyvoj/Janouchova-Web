8fd49c74cbf4e532682d1acdc3a77e31c13baccb
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-05-01 00:20:57 +0200

    sada

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 
 	import Aktuality from "../komponenty/web/Aktuality.svelte";
-	import Carousel from "../komponenty/web/Carousel.svelte";
 	import Karty from "../komponenty/web/Karty.svelte";
 import ONas from "../komponenty/web/O-nas.svelte";
-        
+import Header from "../komponenty/web/Header.svelte";
+import Deti from "../komponenty/web/Deti.svelte";
 
 </script>
+<body  class="bg-fixed bg-no-repeat bg-cover bg-center" style="background-image: url('https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/POZADI-1.gif?alt=media&token=1484df81-b28b-429b-af9a-57f2a3ca7647')">
+
+	
+<Header/>
+<Deti/>
 
 <h1 class="text-6xl font-bold text-center text-black">Váš praktický lékař pro děti a dorost.</h1>
 
@@ -21,3 +26,4 @@ import ONas from "../komponenty/web/O-nas.svelte";
 <footer>
 
 </footer>
+</body>
--- src/routes/administrace/+page.svelte
+++ src/routes/administrace/+page.svelte
@@ -1,7 +1,85 @@
 <script lang="ts">
-
+    import { onMount } from 'svelte';
+    import { createPopper } from '@popperjs/core';
     
-    
-</script>
-
-TEST
+    let showModal = false;
+    let popperInstance;
+  
+    function toggleModal() {
+      showModal = !showModal;
+      if (showModal) {
+        popperInstance = createPopper(
+          $refs.trigger,
+          $refs.modal,
+          {
+            placement: 'bottom',
+            modifiers: [
+              {
+                name: 'offset',
+                options: {
+                  offset: [0, 10],
+                },
+              },
+            ],
+          }
+        );
+      } else {
+        popperInstance.destroy();
+      }
+    }
+  
+    onMount(() => {
+      // Initialize Popper.js on modal and trigger refs
+      popperInstance = createPopper(
+        $refs.trigger,
+        $refs.modal,
+        {
+          placement: 'bottom',
+          modifiers: [
+            {
+              name: 'offset',
+              options: {
+                offset: [0, 10],
+              },
+            },
+          ],
+        }
+      );
+    });
+  </script>
+  
+  <div class="flex justify-center items-center h-screen">
+    <div class="space-x-4">
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" on:click={toggleModal}>
+        Button 1
+      </button>
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" on:click={toggleModal}>
+        Button 2
+      </button>
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" on:click={toggleModal}>
+        Button 3
+      </button>
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" on:click={toggleModal}>
+        Button 4
+      </button>
+    </div>
+  
+    {#if showModal}
+      <div class="fixed inset-0 bg-black opacity-50"></div>
+      <div class="fixed top-0 left-0 flex justify-center items-center w-full h-full z-50">
+        <div class="bg-white rounded-lg shadow-lg p-4" ref:modal>
+          <h2 class="text-lg font-bold mb-4">Modal</h2>
+          <textarea class="w-full h-24 border border-gray-300 p-2 rounded-lg" placeholder="Enter some text"></textarea>
+        </div>
+      </div>
+    {/if}
+  </div>
+  
+  <style>
+    /* Styles for the modal */
+    [data-popper-placement^="bottom"] > .popper,
+    [data-popper-placement^="bottom"] > .tooltip {
+      margin-top: 10px;
+    }
+  </style>
+  
