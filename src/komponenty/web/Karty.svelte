5959b566526415b9e99a292041d251cb2195404f
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-04-30 23:03:49 +0200

    Ver 3.0

--- src/komponenty/web/Karty.svelte
+++ src/komponenty/web/Karty.svelte
@@ -3,34 +3,30 @@
 
 </script>
 <div class="flex justify-center items-center h-screen">
-    <div class="flex space-x-4">
-      <div class="w-64 h-96 bg-yellow-200 rounded-lg shadow-md flex flex-col items-center justify-center">
-        <div class="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8 text-white">
-            <path fill-rule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zM7.293 7.293a1 1 0 011.414 0L10 8.586l1.293-1.293a1 1 0 011.414 1.414L11.414 10l1.293 1.293a1 1 0 01-1.414 1.414L10 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L8.586 10 7.293 8.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-          </svg>
+    <div class="grid grid-cols-3 gap-12">
+      <div class="w-96 h-96  bg-pink-200 rounded-lg shadow-md flex flex-col items-center justify-center">
+        <div class="w-24 h-24 bg-transparent rounded-full flex items-center justify-center">
+          <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/asdasds.png?alt=media&token=fe20a4ea-83ca-47c3-b71a-8f5de6c6d781" alt="ikonka 1" class="w-24 h-24 mt-24 rounded-full object-cover object-center">
         </div>
-        <h3 class="text-lg font-medium mt-4">Nadpis karty 1</h3>
-        <p class="mt-2 text-gray-500 text-center">Popis karty 1</p>
+        <h3 class=" mt-16 text-lg font-medium flex-grow text-center">Ordinační doba</h3>
+        <p class="mt-14 text-gray-500 flex-grow text-center" style="line-height: 1.5rem;">PO: 7:00 - 12:00 / 13:00 - 14:30 <br>ÚT: 7:00 - 12:00 / 12:30 - 13:30 <br>ST: 7:00 - 12:00 / 12:30 - 13:30 <br>ČT: 7:00 - 12:00 / 12:30 - 13:30 <br>PÁ: 7:00 - 12:00</p>
       </div>
-      <div class="w-64 h-96 bg-yellow-200 rounded-lg shadow-md flex flex-col items-center justify-center">
-        <div class="w-16 h-16 bg-red-500 rounded-full flex items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8 text-white">
-            <path fill-rule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zM7.293 7.293a1 1 0 011.414 0L10 8.586l1.293-1.293a1 1 0 011.414 1.414L11.414 10l1.293 1.293a1 1 0 01-1.414 1.414L10 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L8.586 10 7.293 8.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-          </svg>
+      <div class="w-96 h-96 bg-green-200 rounded-lg shadow-md flex flex-col items-center justify-center">
+        <div class="w-24 h-24 bg-transparent rounded-full flex items-center justify-center">
+          <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/dsad.png?alt=media&token=5e960488-2e2c-4f0b-a5bf-807bdddcdc54" alt="ikonka 2" class="w-24 h-24 mt-24 rounded-full object-cover object-center">
         </div>
-        <h3 class="text-lg font-medium mt-4">Nadpis karty 2</h3>
-        <p class="mt-2 text-gray-500 text-center">Popis karty 2</p>
+        <h3 class=" mt-16 text-lg font-medium flex-grow text-center mt-6">Kontakt</h3>
+        <p class="mt-1 text-gray-500 flex-grow text-center" style="line-height: 1.5rem;">Telefon: +420 380 741 055 <br>E-mail: mudrjanouchova@seznam.cz<br>IČO:72038594</p>
       </div>
-      <div class="w-64 h-96 bg-yellow-200 rounded-lg shadow-md flex flex-col items-center justify-center">
-        <div class="w-16 h-16 bg-red-500 rounded-full flex items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8 text-white">
-            <path fill-rule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zM7.293 7.293a1 1 0 011.414 0L10 8.586l1.293-1.293a1 1 0 011.414 1.414L11.414 10l1.293 1.293a1 1 0 01-1.414 1.414L10 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L8.586 10 7.293 8.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-          </svg>
+      <div class="w-96 h-96 bg-blue-200 rounded-lg shadow-md flex flex-col items-center justify-center">
+        <div class="w-24 h-24 bg-transparent rounded-full flex items-center justify-center">
+          <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/asda.png?alt=media&token=335136dc-6e8e-4a56-a81c-1e2ba06f13bd" alt="ikonka 3" class="w-24 h-24 mt-24 rounded-full object-cover">
         </div>
-        <h3 class="text-lg font-medium mt-4">Nadpis karty 3  </h3>
-        <p class="mt-2 text-gray-500 text-center">Popis karty 3</p>
-      </div>
-      </div>
-      </div>
-      
+        <h3 class="mt-16 text-lg font-medium flex-grow text-center mt-6">Adresa</h3>
+        <p class="mb-10 text-gray-500 flex-grow text-center">Budějovická 45, 382 03 Křemže</p>
+        </div>    
+          </div>
+        </div>
+  
+  
+  
