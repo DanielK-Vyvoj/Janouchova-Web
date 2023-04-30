0b6947b8e5cb6c174ef6857416ea4531798bb28f
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-05-01 00:37:29 +0200

    dsda

--- src/routes/administrace/+page.svelte
+++ src/routes/administrace/+page.svelte
@@ -1,37 +1,60 @@
-<script lang="ts">
-    import { Button } from "carbon-components-svelte";
+<style>
+
+    main {
+      height: 100vh;
+    }
+  
+    .button-separator {
+      height: 1px;
+      background-color: #dcdcdc;
+      margin: 8px 0;
+    }
+  
+    button {
+      margin-bottom: 8px;
+    }
+  </style>
+  
+  <script lang="ts">
+    import { Button, Modal } from "carbon-components-svelte";
   
     const buttons = [
-      { label: "Upravit Aktulity" },
-      { label: "Upravit Novinky" },
-      { label: "Upravit Ordinační dobu" },
-      { label: "Upravit kontakt" },
+      { label: "Upravit O nás", modalContent: "Upravit O nás" },
+      { label: "Upravit Aktuality", modalContent: "Upravit Aktuality" },
+      { label: "Upravit Ordinační dobu", modalContent: "Upravit Ordinační dobu" },
+      { label: "Upravit Kontakt", modalContent: "Upravit Kontak" },
+      { label: "Upravit Adresu", modalContent: "Upravit Adresu" },
+      { label: "Upravit Služby", modalContent: "Upravit Služby" },
+      { label: "Upravit Ceník", modalContent: "Upravit Ceník" },
     ];
+  
+    let currentModalContent = "";
+    let currentModalOpen = false;
+  
+    function openModal(modalContent: string) {
+      currentModalContent = modalContent;
+      currentModalOpen = true;
+    }
+  
+    function closeModal() {
+      currentModalContent = "";
+      currentModalOpen = false;
+    }
   </script>
   
   <main class="flex flex-col items-center justify-center">
     {#each buttons as button, index}
-      <Button class="mb-8">{button.label}</Button>
+      <Button class="mb-8" on:click={() => openModal(button.modalContent)}>
+        {button.label}
+      </Button>
       {#if index !== buttons.length - 1}
         <div class="button-separator"></div>
       {/if}
     {/each}
-  </main>
-
-  <style>
-   main {
-    height: 100vh;
-  }
-
-  .button-separator {
-    height: 1px;
-    background-color: #dcdcdc;
-    margin: 8px 0;
-  }
-
-  button {
-    margin-bottom: 8px;
-  }
-  </style>
   
+    <Modal bind:open={currentModalOpen} on:close={closeModal}>
+      <h3 class="mb-4">{currentModalContent}</h3>
+      <Button on:click={closeModal}>Close</Button>
+    </Modal>
+  </main>
   
