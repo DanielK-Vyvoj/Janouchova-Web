97c918131050a53447a7ff0ff2e190f0feabcb9c
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-04-30 21:10:16 +0200

    d

--- src/komponenty/web/O-nas.svelte
+++ src/komponenty/web/O-nas.svelte
@@ -4,7 +4,7 @@
       <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/2.png?alt=media&token=4b592f51-b7f6-4f21-9d36-cd65b3edf7e3" alt="obrazek" class="h-46 w-1/3 object-cover">
       <div class="w-2/3 px-8 py-2">
         <h2 class="text-4xl font-bold mb-2 text-center text-black font-semibold">O nás</h2>
-        <p class="text-gray-900 text-base text-center py-13 ">Naše ordinace se stará o zdraví dětí od novorozenců až do věku 19 let. Najdete u nás vždy příjemný přístup a dobře naladěnou atmosféru. Služby, pro které si k nám můžete přijít najdete sepsané v záložce “Služby“.</p>
+        <p class="text-gray-900 text-base text-center py-12 ">Naše ordinace se stará o zdraví dětí od novorozenců až do věku 19 let. Najdete u nás vždy příjemný přístup a dobře naladěnou atmosféru. Služby, pro které si k nám můžete přijít najdete sepsané v záložce “Služby“.</p>
       </div>
     </div>
   </div>
