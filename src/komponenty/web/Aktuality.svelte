f758da62adb858ee57ca55f77cc481792c58447a
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-04-30 22:01:20 +0200

    kj

--- src/komponenty/web/Aktuality.svelte
+++ src/komponenty/web/Aktuality.svelte
@@ -2,10 +2,10 @@
 <div class="flex items-center justify-center py-16">
     <div class="relative flex bg-white rounded-lg shadow-lg overflow-hidden w-24/36 lg:w-1/2">
       <div class="absolute w-full h-full top-0 left-0 border-4 border-transparent rounded-lg animate-border-pulse"></div>
-      <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/1.png?alt=media&token=76af979e-8cbb-4cff-ab1e-f48a4b0dfef1" alt="obrazek" class="h-46 w-1/3 object-cover">
+      <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/1.png?alt=media&token=7fd524d4-7725-4def-9cda-9c348e7669be" alt="obrazek" class="h-46 w-1/3 object-cover">
       <div class="w-2/3 px-8 py-2">
-        <h2 class="text-4xl font-bold mb-2 text-center text-black font-semibold">Aktuality</h2>
-        <p class="text-gray-900 text-base text-center py-14 ">Momentálně nemáme žádné aktuality</p>
+        <h2 class="text-4xl font-bold mb-2 text-center text-purple-500 font-semibold">Aktuality</h2>
+        <p class="text-gray-900 text-base text-center ">Momentálně nemáme žádné Aktuality</p>
       </div>
     </div>
   </div>
