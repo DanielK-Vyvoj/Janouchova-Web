858bd833ff45114362f825ab720287203c591cfe
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-05-01 02:56:17 +0200

    sd

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIpTv9uEG4bSjDuz9s2KPsrfOaIZ9Gl3Q",
-  authDomain: "jk-app-24506.firebaseapp.com",
-  databaseURL: "https://jk-app-24506-default-rtdb.firebaseio.com",
-  projectId: "jk-app-24506",
-  storageBucket: "jk-app-24506.appspot.com",
-  messagingSenderId: "553191098818",
-  appId: "1:553191098818:web:bbc45749d926ac5376ae5b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
