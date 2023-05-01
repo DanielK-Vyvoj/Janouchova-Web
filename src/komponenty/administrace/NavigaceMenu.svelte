a71f79bec290492d68ab29005112f964755268b4
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-05-01 10:12:46 +0200

    asd

--- src/komponenty/administrace/NavigaceMenu.svelte
+++ src/komponenty/administrace/NavigaceMenu.svelte
@@ -32,12 +32,14 @@
           </button>
         </div>
         <nav class="flex-grow pb-4 pr-4 pl-6">
-          <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-gray-200 rounded-lg hover:bg-gray-300" href="/dashboard">Dashboard</a>
+          <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace">Admninistrační část</a>
           <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/aktuality">Upravit Aktuality</a>
           <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/ordinace-doba">Upravit Ordinační dobu</a>
           <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/onas">Upravit O nás</a>
           <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/cenik">Upravit Ceník</a>
           <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/kontakt">Upravit Kontakt</a>
+          <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/adresa">Upravit Adresu</a>
+          <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300" href="/administrace/sluzby">Upravit Služby</a>
         </nav>
       </div>
     </div>
