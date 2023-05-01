23fc8ad0bb608d639482ac655bb77f6775369a4b
Daniel Krcil <daniel.kvyvoj@gmail.com>
2023-05-01 20:31:34 +0200

    asd

--- src/komponenty/web/Header.svelte
+++ src/komponenty/web/Header.svelte
@@ -94,6 +94,11 @@
       `;
       document.head.appendChild(style);
     });
+    function handleClick() {
+    // Zde můžete vložit kód, který zavolá na dané telefonní číslo
+    // Například:
+    window.location.href = 'tel:+420 380 741 055';
+  }
 </script>
 
 <nav class="navbar fixed top-0 left-0 w-full z-10 bg-opacity-50 backdrop-filter backdrop-blur-md p-4">
@@ -107,7 +112,7 @@
         <li><a href="/cenik" class="text-white hover:bg-green-500 hover:text-white transition duration-300 ease-in-out rounded-lg py-1 px-3 bg-green-400">Ceník</a></li>
         <li><a href="/deti" class="text-white hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg py-1 px-3 bg-blue-400">Pro děti</a></li>
       </ul>
-      <button class="animate-btn bg-green-500 text-white font-bold py-2 px-4 rounded mr-36">
+      <button class="animate-btn bg-green-500 text-white font-bold py-2 px-4 rounded mr-36" on:click={handleClick}>
         Zavolejte do ordinace
       </button>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@ import Deti from "../komponenty/web/Deti.svelte";
 <ONas/>
 <Aktuality/>
 <Karty/>
-<h1 class="text-3xl mt-16 font-bold text-center text-black">Seznam smluvních pojišťoven</h1>
+<h1 class="text-3xl mt-16 font-bold text-center text-black">Seznam smluvních pojišťoven:</h1>
 <div class="w-full">
     <img src="https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/pji.png?alt=media&token=8d3e2883-df3a-4c14-8ae7-ab1000eed008" alt="Děti" class="w-1591.2 h-451 object-contain">
 </div>
