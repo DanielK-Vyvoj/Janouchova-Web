7cf30d1ae355104e014a7dfba6f7e4a90bd7f357
daniel.krcil <daniel.kvyvoj@gmail.com>
2023-05-03 18:26:40 +0200

    dds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 
-import Aktuality from "../komponenty/web/Aktuality.svelte";
-import Karty from "../komponenty/web/Karty.svelte";
-import ONas from "../komponenty/web/O-nas.svelte";
-import Header from "../komponenty/web/Header.svelte";
-import Deti from "../komponenty/web/Deti.svelte";
+
 
 </script>
 <body  class="bg-fixed bg-no-repeat bg-cover bg-center" style="background-image: url('https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/POZADI-1.gif?alt=media&token=1484df81-b28b-429b-af9a-57f2a3ca7647')">
