24b1d2fa0066f1540d568e33edf706ce68b33764
daniel.krcil <daniel.kvyvoj@gmail.com>
2023-05-04 00:31:02 +0200

    sda

--- src/routes/cenik/+page.svelte
+++ src/routes/cenik/+page.svelte
@@ -19,7 +19,7 @@ onValue(cenikRef, (snapshot) => {
 
 </script>
 
-<body  class="bg-fixed bg-no-repeat bg-cover bg-center" style="background-image: url('https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/POZADI-1.gif?alt=media&token=1484df81-b28b-429b-af9a-57f2a3ca7647')">
+<body  class=" min-h-screena bg-fixed bg-no-repeat bg-cover bg-center" style="background-image: url('https://firebasestorage.googleapis.com/v0/b/jk-app-24506.appspot.com/o/POZADI-1.gif?alt=media&token=1484df81-b28b-429b-af9a-57f2a3ca7647')">
   <Header/>
   <Deti/>
   <h1 class="text-6xl font-bold text-center text-black">Ceník</h1>
